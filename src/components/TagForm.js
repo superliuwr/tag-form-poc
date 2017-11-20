39f5486f604f7b65cff0b9600ac8a850fa250480
Marvin Liu <superliuwr@gmail.com>
2017-11-20 22:42:41 +1100

    Initial commit

---
+++ src/components/TagForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './TagForm';
+
+class TagForm extends Component {
+  render() {
+    return (
+      <p className="App-intro">
+        This is going to be replaced with a form.
+      </p>
+    );
+  }
+}
+
+export default TagForm;
